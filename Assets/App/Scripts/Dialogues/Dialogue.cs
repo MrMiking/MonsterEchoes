4f658d91bd6d3b6166faeed79260b6ee9c2976f3
Tristan Poveda <povedatristan@gmail.com>
2026-01-26 15:47:23 +0100

    Interact with pnj

--- Assets/App/Scripts/Dialogues/Dialogue.cs
+++ Assets/App/Scripts/Dialogues/Dialogue.cs
@@ -1,12 +1,55 @@
+using System;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Dialogue : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private SSO_Dialogue dialogueData;
     [SerializeField] private RSE_SendDialogue sendDialogue;
+    [SerializeField] private GameObject inputVisual;
+    
+    [Header("Inputs")]
+    [SerializeField] private InputActionReference interactInput;
 
-    public void Interact()
+    private bool canInteract;
+
+    private void Awake()
     {
+        inputVisual.SetActive(false);
+        
+        interactInput.action.Enable();
+    }
+
+    private void OnEnable()
+    {
+        interactInput.action.performed += TryInteract;
+    }
+
+    private void OnDisable()
+    {
+        interactInput.action.performed -= TryInteract;
+    }
+
+    private void TryInteract(InputAction.CallbackContext context)
+    {
+        if (!canInteract) return;
+        
+        inputVisual.SetActive(false);
+        
         sendDialogue.Call(dialogueData);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) canInteract = true;
+        inputVisual.SetActive(canInteract);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) canInteract = false;
+        inputVisual.SetActive(canInteract);
     }
 }
