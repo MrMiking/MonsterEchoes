98820a00585dc0850cf60d893a5cac0394787db9
Steamcollect <eliot.garnier.080805@gmail.com>
2026-01-27 10:23:17 +0100

    Add health to prefabs

---
+++ Assets/App/Scripts/Boss/BossHealth.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BossHealth : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] int maxHealth;
+
+    [Header("References")]
+    [SerializeField] BossVisual visual;
+    [SerializeField] RSO_Bosshealth currentHealth;
+
+    //[Header("Input")]
+    [Header("Output")]
+    [SerializeField] UnityEvent OnTakeDamage;
+    [SerializeField] UnityEvent OnDeath;
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth.Set(currentHealth.Get() - damage);
+
+        if (currentHealth.Get() <= 0)
+        {
+            Die();
+            return;
+        }
+
+        OnTakeDamage?.Invoke();
+    }
+
+    public void Die()
+    {
+        OnDeath?.Invoke();
+    }
+}
---
+++ Assets/App/Scripts/Boss/RSO/RSO_Bosshealth.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+using MVsToolkit.Wrappers;
+
+[CreateAssetMenu(fileName = "RSO_Bosshealth", menuName = "RSO/Boss/RSO_Bosshealth")]
+public class RSO_Bosshealth : RuntimeScriptableObject<int>{}
