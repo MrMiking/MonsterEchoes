d28379267851da03b3467e4ba6bc69d70b5d6265
Steamcollect <eliot.garnier.080805@gmail.com>
2026-01-28 15:31:36 +0100

    Parallax

---
+++ Assets/App/Scripts/Props/ParallaxObject.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ParallaxObject : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float parallaxSpeedX; // Vitesse sur l'axe X (plus petit = plus lointain)
+    [SerializeField] private float parallaxSpeedY; // Vitesse sur l'axe Y
+
+    private Vector3 startPosition = Vector3.zero;
+    private Vector3 startCamPosition = Vector3.zero;
+    private Camera cam = null;
+
+    private void Start()
+    {
+        cam = Camera.main;  // Utilise automatiquement la caméra principale
+
+        startPosition = transform.position;
+        startCamPosition = cam.transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 camDelta = cam.transform.position - startCamPosition + new Vector3(-100, 0, 0);
+
+        // Applique la vitesse de parallaxe différente pour chaque axe
+        transform.position = startPosition + new Vector3(camDelta.x * parallaxSpeedX, camDelta.y * parallaxSpeedY, 0);
+    }
+}
